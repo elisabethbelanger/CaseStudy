054785a9f874d2d9a9962a7642ed394026212a7a
Elisabeth Belanger <89950647+elisabethbelanger@users.noreply.github.com>
2022-03-08 10:36:19 -0600

    updated

--- Case Studies/CS1.m
+++ Case Studies/CS1.m
@@ -2,152 +2,184 @@
 % * Names:                  Morgan Davies, Ouyang Du, & Elisabeth Belanger
 % * Date:                   Created 2/27/2022
 
+%% (1) Audio presets
 
-%[xv,xvfs] = audioread('roosevelt_noisy.wav'); len = 20;% 20 sec
-%[xv,xvfs] = audioread('piano_noisy.wav'); len = 12; % 12 sec
+% use the function 'presets' to get the preset gain values
+% input -> 0 - unity, 1 - bass, 2 - treble
+% output -> [g1, g2, g3, g4, g5]
+%
+% g1: 0 - 60
+% g2: 60 - 680
+% g3: 680 - 3320
+% g4: 3320 - 12680
+% g5: 12680+
 
-%[xv,xvfs] = audioread('Blue in Green with Siren.wav'); 
-%[xv,xvfs] = audioread('Space Station - Treble Cut.wav');  
+%% (2A) Giant Steps 
+
 [xv,xvfs] = audioread('Giant Steps Bass Cut.wav'); 
 
-% 0 - unity, 1 - bass, 2 - treble
+% Bass Boost:
 [g1, g2, g3, g4, g5] = presets(1);
-y = equilizer(xv(:, 1), xvfs, g1, g2, g3, g4, g5);
+giant_bass = equilizer(xv(:, 1), xvfs, g1, g2, g3, g4, g5);
+%soundsc(giant_bass, xvfs)
+
+% Treble Boost:
 [g1, g2, g3, g4, g5] = presets(2);
-y2 = equilizer(xv(:, 1), xvfs, g1, g2, g3, g4, g5);
-t = 0:length(xv)-1;
-t = (1/xvfs)*t;
+giant_treble = equilizer(xv(:, 1), xvfs, g1, g2, g3, g4, g5);
+%soundsc(giant_treble, xvfs)
+
+% Unity
+[g1, g2, g3, g4, g5] = presets(0);
+giant_unity = equilizer(xv(:, 1), xvfs, g1, g2, g3, g4, g5);
+%soundsc(giant_unity, xvfs)
+
+t = (0:length(xv)-1)*(1/xvfs);
+
+%figure()
+%plot(t, xv(:, 1));
+%title('Giant - Original');
+
+%figure()
+%plot(t, gaint_bass);
+%title('Giant - Bass Boost');
+
+%figure()
+%plot(t, giant_treble);
+%title('Giant - Treble Boost');
+
+%figure()
+%plot(t, giant_unity);
+%title('Giant - Unity');
 
 
+%% (2B) Space Station 
 
-%% Space Station filtered
-% g1 = 5; % 0 - 60
-% g2 = 1; % 60 - 680
-% g3 = 1; % 680 - 3320
-% g4 = 1; % 3320 - 12680
-% g5 = 1; % 12680+
+[xv,xvfs] = audioread('Space Station - Treble Cut.wav'); 
+
+% Bass Boost:
+[g1, g2, g3, g4, g5] = presets(1);
+space_bass = equilizer(xv(:, 1), xvfs, g1, g2, g3, g4, g5);
+% soundsc(space_bass, xvfs)
+% pause((length(xv)-1)*(1/xvfs)+2)
+% soundsc(xv(:,1), xvfs)
+
+% Treble Boost:
+[g1, g2, g3, g4, g5] = presets(2);
+space_treble = equilizer(xv(:, 1), xvfs, g1, g2, g3, g4, g5);
+soundsc(space_treble, xvfs)
+pause((length(xv)-1)*(1/xvfs)+2)
+soundsc(xv(:,1), xvfs)
+
+% Unity
+[g1, g2, g3, g4, g5] = presets(0);
+% space_unity = equilizer(xv(:, 1), xvfs, g1, g2, g3, g4, g5);
+% soundsc(space_unity, xvfs)
+% pause((length(xv)-1)*(1/xvfs)+2)
+% soundsc(xv(:,1), xvfs)
+
+t = (0:length(xv)-1)*(1/xvfs);
+
+figure()
+plot(t, xv(:, 1)), ylim([-2, 2]);
+title('Space - Original');
+
+figure()
+plot(t, space_bass);
+title('Space - Bass Boost');
+
+figure()
+plot(t, space_treble);
+title('Space - Treble Boost');
+
+figure()
+plot(t, space_unity);
+title('Space - Unity');
+%% (3) Blue in Green
 
 [xv,xvfs] = audioread('Blue in Green with Siren.wav'); 
-y = equilizer(xv(:, 1), xvfs, 10, 0, 0, 0, 0);
-t = 0:length(xv)-1;
-t = (1/xvfs)*t;
+blue_green_filtered = equilizer(xv(:, 1), xvfs, 10, 0, 0, 0, 0);
 
-figure()
-plot(t, y);
-title('filtered');
-figure()
-plot(t, xv(:, 1));
-title('original');
-figure, spectrogram(y,1024,200,1024,xvfs)
-soundsc(y, xvfs)
-pause(15)
-%soundsc(xv, xvfs)
-figure, spectrogram(y,1024,200,1024,xvfs)
-figure, spectrogram(xv(:, 1),1024,200,1024,xvfs)
+t = (0:length(xv)-1)*(1/xvfs);
 
+% figure()
+% plot(t, xv(:, 1));
+% title('Blue in Green - Original');
+% 
+% figure()
+% plot(t, blue_green_filtered);
+% title('Blue in Green - Original');
+% 
+% figure, spectrogram(blue_green_filtered,1024,200,1024,xvfs), title('filtered')
+% figure, spectrogram(xv(:,1),1024,200,1024,xvfs), title('original')
+% 
+% soundsc(blue_green_filtered, xvfs)
+% pause((length(xv)-1)*(1/xvfs)+2)
+% soundsc(xv, xvfs)
 
-%% Giant Steps filtered
-% g1 = 5; % 0 - 60
-% g2 = 1; % 60 - 680
-% g3 = 1; % 680 - 3320
-% g4 = 1; % 3320 - 12680
-% g5 = 1; % 12680+
-
-[xv,xvfs] = audioread('Giant Steps Bass Cut.wav'); 
-y = equilizer(xv(:, 1), xvfs, 10, 0, 0, 0, 0);
-t = 0:length(xv)-1;
-t = (1/xvfs)*t;
-
-figure()
-plot(t, y);
-title('filtered');
-figure()
-plot(t, xv(:, 1));
-title('original');
+%% (4) Enhance a signal
 
 
-soundsc(y, xvfs)
-pause(15)
-%soundsc(xv, xvfs)
-figure, spectrogram(y,1024,200,1024,xvfs), title('filtered');
-figure, spectrogram(xv(:, 1),1024,200,1024,xvfs), title('original');
-
-
-%% Blue in Green filtered
-% g1 = 5; % 0 - 60
-% g2 = 1; % 60 - 680
-% g3 = 1; % 680 - 3320
-% g4 = 1; % 3320 - 12680
-% g5 = 1; % 12680+
-
-[xv,xvfs] = audioread('Blue in Green with Siren.wav'); 
-y = equilizer(xv(:, 1), xvfs, 10, 0, 0, 0, 0);
-t = 0:length(xv)-1;
-t = (1/xvfs)*t;
-
-figure()
-plot(t, y);
-title('filtered');
-figure()
-plot(t, xv(:, 1));
-title('original');
-soundsc(y, xvfs)
-%pause(15)
-%soundsc(xv, xvfs)
-figure, spectrogram(y,1024,200,1024,xvfs)
-figure, spectrogram(xv(:, 1),1024,200,1024,xvfs)
-
-
-%% Frequency responses
-% Low Pass TF
+%% Part 2: (2A) Frequency responses
+% Low-pass TF
 % lp_tf = tf([1],[1 1/tao]);
+
+% High-pass TF
+% hp_tf = tf([1 0],[1 1/tao]);
 
 fc1 = 60;
 tao1 = 1/(2*pi*fc1);
-fs =xvfs;
-% Low pass
-x1 = filter((1/fs)/tao1, [1, (1/fs)/tao1-1], xv);
-freqs([1],[1 1/tao1]);
-%%
-%--------------
-fc2 = 680;
-tao1 = 1/(2*pi*fc1);
-tao2 = 1/(2*pi*fc2);
-fs =xvfs;
 
 % Low pass
-x2 = filter((1/fs)/tao2, [1, (1/fs)/tao2-1], xv);
-freqs([1],[1 1/tao2]);
+figure(), freqs([1],[1 1/tao1]), title('Low Pass - f =', fc1*pi*2);
+
+%-----------------------------------------------------------------------
+%%
+fc2 = 680;
+tao2 = 1/(2*pi*fc2);
+
+% Low pass
+figure()
+freqs(1,[1 1/tao2]), title('Low Pass - f =', fc2*pi*2);
+[H1, W1] = freqs(1,[1 1/tao2]);% title('Low Pass - f =', fc2*pi*2);
 
 % High pass
-x2 = filter([1, -1], [(1+(1/fs)/tao2), -1], x2);
+figure()
+freqs([1 0],[1 1/tao1], W1), title('High Pass - f =', fc1*pi*2);
+[H2, W2] = freqs([1 0],[1 1/tao1], W1);% title('High Pass - f =', fc1*pi*2);
+
+H3 = H1.*H2;
+figure()
+subplot(2, 1, 1)
+set(gca, 'XScale', 'log')
+plot(W2, abs(H3)), title('BP1 Magnitude'), set(gca, 'XScale', 'log');
+subplot(2, 1, 2)
+plot(W2, 180*angle(H3)/pi), title('BP1 Phase'), set(gca, 'XScale', 'log');
 %---------------------
+%%
 fc3 = 3320;
 tao3 = 1/(2*pi*fc3);
-fs =xvfs;
 
 % Low pass
-x3 = filter((1/fs)/tao3, [1, (1/fs)/tao3-1], xv);
-freqs([1],[1 1/tao3]);
+figure(), freqs([1],[1 1/tao3]), title('Low Pass - f =', fc3*2*pi);
 
 % High pass
-x3 = filter([1, -1], [(1+(1/fs)/tao3), -1], x3);
+figure(), freqs([1 0],[1 1/tao2]), title('High Pass - f =', fc2*2*pi);
+
 %----------------------
 fc4 = 12680;
 tao4 = 1/(2*pi*fc4);
-fs =xvfs;
 
 % Low pass
-x4 = filter((1/fs)/tao4, [1, (1/fs)/tao4-1], xv);
-freqs([1],[1 1/tao4]);
+figure(), freqs([1],[1 1/tao4]), title('Low Pass - f =', fc4*2*pi);
 
 % High pass
-x4 = filter([1, -1], [(1+(1/fs)/tao3), -1], x4);
-
+figure(), freqs([1 0],[1 1/tao3]), title('High Pass - f =', fc3*2*pi);
 %------------------
 
 % High pass
 x5 = filter([1, -1], [(1+(1/fs)/tao4), -1], xv);
+figure(), freqs([1 0],[1 1/tao4]), title('High Pass - f =',fc4*2*pi);
+
 
 %% LSIM
 % t = 0:0.001:15;
